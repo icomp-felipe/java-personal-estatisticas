df884c6169ed74e9045b743f2a7947618511dc46
Felipe André <fass@icomp.ufam.edu.br>
2020-09-21 00:49:20 -0400

    Tela de troca de senha revista e comentada

--- Estatisticas/src/com/estatisticas/view/TelaMudaSenha.java
+++ Estatisticas/src/com/estatisticas/view/TelaMudaSenha.java
@@ -3,29 +3,35 @@ package com.estatisticas.view;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.sql.SQLException;
 
-import com.estatisticas.model.*;
 import com.phill.libs.ui.*;
+import com.estatisticas.model.*;
 
-/** Classe TelaMudaSenha - contém um diálogo de troca de senha do sistema
- *  @author Felipe André Souza da Silva 
- *  @version 2.6, 19/09/2020 */
+/** Contém um diálogo de troca de senha do sistema.
+ *  @author Felipe André - felipeandresouza@hotmail.com
+ *  @version 2.0, 21/09/2020 */
 public class TelaMudaSenha extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPasswordField textSenhaOnce, textSenhaTwice;
+	// Serial
+	private static final long serialVersionUID = 3483645244458314752L;
 	
-	public static void main(String[] args) {
-		new TelaMudaSenha();
-	}
+	// Constantes
+	private final Usuario usuario;
 	
-	/** Constrói a janela gráfica */
-	public TelaMudaSenha() {
+	// Atributos gráficos
+	private final JPasswordField textSenhaOnce, textSenhaTwice;
+	
+	/** Constrói a janela gráfica. */
+	public TelaMudaSenha(final Usuario usuario) {
 		super("Atualização de Senha");
+		
+		// Registrando usuário
+		this.usuario = usuario;
 		
 		// Inicializando atributos gráficos
 		GraphicsHelper helper = GraphicsHelper.getInstance();
-		//GraphicsHelper.setFrameIcon(this,"img/logo.png");
+		GraphicsHelper.setFrameIcon(this,"img/logo.png");
 						
 		Dimension dimension = new Dimension(300,225);
 		JPanel    mainPanel = new JPaintedPanel("img/background.png",dimension);
@@ -65,11 +71,13 @@ public class TelaMudaSenha extends JFrame {
 		painelConfirmaSenha.add(textSenhaTwice);
 		
 		JButton botaoSalvar = new JButton("Salvar");
+		botaoSalvar.setToolTipText("Salvar alterações");
 		botaoSalvar.addActionListener((event) -> action_update_password());
 		botaoSalvar.setBounds(152, 158, 90, 25);
 		mainPanel.add(botaoSalvar);
 		
 		JButton botaoSair = new JButton("Sair");
+		botaoSair.setToolTipText("Sair desta tela");
 		botaoSair.addActionListener((event) -> dispose());
 		botaoSair.setBounds(56, 158, 90, 25);
 		mainPanel.add(botaoSair);
@@ -85,18 +93,31 @@ public class TelaMudaSenha extends JFrame {
 		setVisible(true);
 	}
 	
-	/** Verifica as senhas e as altera no banco de dados */
+	/** Verifica as senhas e as altera no banco de dados. */
 	private void action_update_password() {
 		
-		String firstKey  = new String(textSenhaOnce .getPassword());
-		String secondKey = new String(textSenhaTwice.getPassword());
+		final String title = "Atualizando senha";
+		
+		final String firstKey  = new String(textSenhaOnce .getPassword());
+		final String secondKey = new String(textSenhaTwice.getPassword());
 		
 		if (firstKey.equals(secondKey)) {
-			UsuarioDAO.changePassword(secondKey);
-			dispose();
+			
+			try {
+				
+				UsuarioDAO.changePassword(secondKey);	this.usuario.setSenha(secondKey);
+				AlertDialog.info(title, "Senha de acesso atualizada com sucesso!");
+				dispose();
+				
+			}
+			catch (SQLException exception) {
+				exception.printStackTrace();
+				AlertDialog.error(title, "Falha ao alterar senha de acesso");
+			}
+			
 		}
 		else
-			AlertDialog.error("Atualizando senha","Senhas não conferem!");
+			AlertDialog.error(title,"Senhas não conferem!");
 		
 	}
 
